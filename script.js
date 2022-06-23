a3b12811a1ad93980c95d7c7391ba7503845d898
Daniel Alves <danielalves.silva@hotmail.com>
2022-06-23 11:29:56 -0300

    Meu Primeiro Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let controle1 = document.querySelector('#controle');
+let nomeCliente = document.querySelector('.nome-1');
+let nomeEndereco = document.querySelector('.endereço');
+let pesqCliente = document.querySelector('.clientenome');
+let lerCliente = document.querySelector('.nome-2');
+let saldoAutal = document.querySelector('.s-atual');
+let maisSaldo = document.querySelector('.a-acrescentar');
+let diminSaldo = document.querySelector('.d-diminuir');
+let novoSaldo = document.querySelector('.n-saldo');
+
+function btncadastrar() {
+    alert('Clicou no botão Cadastrar')
+}
+
+function btnpesquisar() {
+    alert('clicou em Pesquisar')
+};
+
+function btnsomar() {
+    alert ('Clicou em Somar')
+};
+
+function btndiminuir() {
+    alert ("clicou em Diminuir")
+};
+
+function btnsaldo() {
+    alert ('Clicou em Saldo')
+};
